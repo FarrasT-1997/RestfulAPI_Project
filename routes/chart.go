47e63465798dff122bcc2cf067aac73f058222b7
ihsan-null <ihsanulz92@gmail.com>
2021-07-29 11:45:26 +0700

    route files got merged into routes.go

--- routes/chart.go
+++
@@ -1,8 +0,0 @@
-package routes
-
-func ChartRoutes(e *echo.Echo) {
-	e.GET("/cart", GetAllProductInChart)
-	e.POST("/cart/:nameproduct", InsertProductIntoChart)
-	e.DELETE("/cart/:nameproduct", DeleteProductInChart)
-	e.PUT("/cart/:nameproduct", ChangeProductStockInCart)
-}
--- routes/user.go
+++
@@ -1,8 +0,0 @@
-package routes
-
-func UserRoutes(e *echo.Echo) {
-	e.GET("/jwt/users", GetAllUsers)
-	e.POST("/login", Login)
-	e.POST("/signup", SignUpUser)
-	e.PUT("/jwt/users/:userid", ChangeProfile)
-}
