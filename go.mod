6b996ef38dd04055fb2f0de370bc8b3439ff665b
AnggunPermata <anggunpermata.g789@gmail.com>
2021-07-29 11:43:41 +0700

    add routes

---
+++ go.mod
@@ -0,0 +1,9 @@
+module project/team-alta/api-ecommerce
+
+go 1.16
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"project/team-alta/api-ecommerce/config"
+	"project/team-alta/api-ecommerce/routes"
 
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	config.Init_DB()
+
+	routes.CategoryRoutes(e)
+	routes.ChartRoutes(e)
+	routes.TransactionDetail(e)
+	routes.Transaction(e)
+	routes.UserRoutes(e)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
