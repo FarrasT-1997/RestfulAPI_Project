a8a9303a2ebabc2ebe444b07205c9dd56947b5e0
AnggunPermata <anggunpermata.g789@gmail.com>
2021-07-30 11:24:16 +0700

    add user's database and modifying user's models

--- lib/database/user.go
+++ lib/database/user.go
@@ -2,12 +2,54 @@ package database
 
 import (
 	"restfulAPI/config"
+	"restfulAPI/middlewares"
 	"restfulAPI/models"
 )
 
 func SignUp(user models.User) (interface{}, error) {
 	if err := config.DB.Save(&user).Error; err != nil {
 		return nil, err
+	}
+	return user, nil
+}
+
+////////////////////////////////////////////////////////
+
+func CreateUser(user models.User) (interface{}, error) {
+	if err := config.DB.Save(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func LoginUsers(email, password string) (interface{}, error) {
+	var err error
+	var user models.User
+	if err = config.DB.Where("email=? AND password=?", email, password).First(user).Error; err != nil {
+		return nil, err
+	}
+
+	user.Token, err = middlewares.CreateToken(int(user.ID))
+	if err != nil {
+		return nil, err
+	}
+	if err := config.DB.Save(user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func GetOneUser(id int) (models.User, error) {
+	var user models.User
+	if err := config.DB.Find(&user, "id=?", id).Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func EditUser(user models.User) (interface{}, error) {
+	if err := config.DB.Save(&user).Error; err != nil {
+		return user, err
 	}
 	return user, nil
 }
